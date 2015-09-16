949c4ecce3cc4d07837765a7e1699d9c3a4767c2
Ethan Steiner <ethans@keimlumber.com>
2015-09-16 07:42:25 -0400

    Lesson 4

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -2,8 +2,6 @@
 {
     using Akka.Actor;
 
-    #region Program
-
     internal class Program
     {
         public static ActorSystem MyActorSystem;
@@ -16,7 +14,11 @@
             Props consoleWriterProps = Props.Create(() => new ConsoleWriterActor());
             IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
-            Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            // make tailCoordinator Actor
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+
+            Props validationActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
             IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
 
             Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
@@ -29,6 +31,4 @@
             MyActorSystem.AwaitTermination();
         }
     }
-
-    #endregion
 }
