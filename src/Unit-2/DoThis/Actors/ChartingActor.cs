0ac03cd2ac70236598905eabcf143e2f1427f249
Ethan Steiner <ethans@keimlumber.com>
2015-09-21 08:42:19 -0400

    Lesson 3 Complete

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,14 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms.DataVisualization.Charting;
-using Akka.Actor;
-
-namespace ChartApp.Actors
+﻿namespace ChartApp.Actors
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms.DataVisualization.Charting;
+    using Akka.Actor;
+
     public class ChartingActor : ReceiveActor
     {
-        #region Messages
+        /// <summary>
+        ///     Maximum number of points we will allow in a series
+        /// </summary>
+        public const int MaxPoints = 250;
+
+        private readonly Chart _chart;
+        private Dictionary<string, Series> _seriesIndex;
+
+        /// <summary>
+        ///     Incrementing counter we use to plot along the X-axis
+        /// </summary>
+        private int xPosCounter = 0;
+
+        public ChartingActor(Chart chart)
+            : this(chart, new Dictionary<string, Series>())
+        {
+        }
+
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        {
+            _chart = chart;
+            _seriesIndex = seriesIndex;
+
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
+        }
 
         public class InitializeChart
         {
@@ -21,7 +48,7 @@ namespace ChartApp.Actors
         }
 
         /// <summary>
-        /// Add a new <see cref="Series"/> to the chart
+        ///     Add a new <see cref="Series" /> to the chart
         /// </summary>
         public class AddSeries
         {
@@ -33,25 +60,18 @@ namespace ChartApp.Actors
             public Series Series { get; private set; }
         }
 
-        #endregion
-
-        private readonly Chart _chart;
-        private Dictionary<string, Series> _seriesIndex;
-
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
+        /// <summary>
+        /// Remove an existing <see cref="Series"/> from the chart
+        /// </summary>
+        public class RemoveSeries
         {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
         }
-
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
-        {
-            _chart = chart;
-            _seriesIndex = seriesIndex;
-
-            Receive<InitializeChart>(ic => HandleInitialize(ic));
-            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
-        }
-
-        #region Individual Message Type Handlers
 
         private void HandleAddSeries(AddSeries series)
         {
@@ -60,6 +80,7 @@ namespace ChartApp.Actors
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
             }
         }
 
@@ -69,12 +90,19 @@ namespace ChartApp.Actors
             {
                 //swap the two series out
                 _seriesIndex = ic.InitialSeries;
-            }
+            }  
 
-            //delete any existing series
+            // delete any existing series
             _chart.Series.Clear();
 
-            //attempt to render the initial chart
+            //set the axes up
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
+            // attempt to render the initial chart
             if (_seriesIndex.Any())
             {
                 foreach (var series in _seriesIndex)
@@ -84,8 +112,51 @@ namespace ChartApp.Actors
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
         }
 
-        #endregion
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) &&
+                _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) &&
+                _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
     }
 }
